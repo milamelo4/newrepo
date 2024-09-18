b1a35feeb21974ffa15928e66f993375ae4e2109
milamelo4 <mel19009@byui.edu>
2024-09-18 15:19:55 -0600

    Build app, EJS View, index route w/ express

--- server.js
+++ server.js
@@ -6,14 +6,27 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts") //require express-ejs-layouts, so it can be used.
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 
 /* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs") // declare that ejs will be the view engine
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
+
+/* ***********************
  * Routes
  *************************/
 app.use(static)
+// Index routes being watched
+// "get" object, within the HTTP Request, for a particular route.
+app.get('/', function(req, res) {
+  res.render("index", { title: "Home" }); // render- function that will retrieve the specified view "index" to be sent back to the browser.
+})
 
 /* ***********************
  * Local Server Information
