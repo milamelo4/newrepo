717f3f646044eb459ca148409fbb0a3efa1aa776
milamelo4 <mel19009@byui.edu>
2024-09-25 17:40:12 -0600

    Worked on team activity and started assignment week02

---
+++ database/assignment2.sql
@@ -0,0 +1,24 @@
+-- 1-The Tony Stark insert SQL statement works.
+insert into public.account 
+values(DEFAULT,'Tony', 'Stark', 'tony@starkent.com', 'Iam1ronM@n');
+
+-- 2-Modify the Tony Stark record to change the account_type to `Admin`
+Update public.account
+set account_type = 'Admin'
+where account_id = '1';
+
+-- 3-Delete the Tony Stark record from the database
+delete from public.account
+where account_id = '1';
+
+-- 4-The description update SQL statement works.
+Update public.inventory
+set inv_description = replace(inv_description, 'small interiors', 'a huge interior')
+where inv_make = 'GM' and inv_model = 'Hummer';
+
+-- 5-The select query using a JOIN SQL statement works
+select inv_model, inv_make, cla.classification_name
+from public.inventory as inv
+inner join public.classification as cla 
+on inv.classification_id = cla.classification_id
+where cla.classification_name = 'Sport';
