63d8bdde47c45ef04f83377ecbd784edf2f5f364
milamelo4 <mel19009@byui.edu>
2024-10-02 15:36:06 -0600

    started css for inventory

--- utilities/index.js
+++ utilities/index.js
@@ -6,6 +6,7 @@ const Util = {};
  ************************** */
 Util.getNav = async function (req, res, next) {
   let data = await invModel.getClassifications();
+  //console.log(data)
   let list = "<ul>";
   list += '<li><a href="/" title="Home page">Home</a></li>';
   data.rows.forEach((row) => {
@@ -24,4 +25,55 @@ Util.getNav = async function (req, res, next) {
   return list;
 };
 
+/* **************************************
+* Build the classification view HTML
+* ************************************ */
+Util.buildClassificationGrid = async function(data){
+  let grid
+  if(data.length > 0){
+    grid = '<ul id="inv-display" class="inv-ul">'
+    data.forEach(vehicle => { 
+      grid += '<li>'
+      grid +=  '<a href="../../inv/detail/'+ vehicle.inv_id 
+      + '" title="View ' + vehicle.inv_make + ' '+ vehicle.inv_model 
+      + 'details"><img src="' + vehicle.inv_thumbnail 
+      +'" alt="Image of '+ vehicle.inv_make + ' ' + vehicle.inv_model 
+      +' on CSE Motors" /></a>'
+      grid += '<div class="namePrice">'
+      
+      grid += '<h2 class="button_cont">'
+      grid +=
+        '<a class="example_d" href="../../inv/detail/' +
+        vehicle.inv_id +
+        '" title="View ' +
+        vehicle.inv_make +
+        " " +
+        vehicle.inv_model +
+        ' details">' +
+        vehicle.inv_make +
+        " " +
+        vehicle.inv_model +
+        " </a>";
+      grid += '</h2>'
+      grid += '<span>$' 
+      + new Intl.NumberFormat('en-US').format(vehicle.inv_price) + '</span>'
+      grid += "<hr />"
+      grid += '</div>'
+      grid += '</li>'
+    })
+    grid += '</ul>'
+  } else { 
+    grid += '<p class="notice">Sorry, no matching vehicles could be found.</p>'
+  }
+  return grid
+}
+
+
+
 module.exports = Util;
+
+// Single Item Returned
+// [objectName.rows[0] - (e.g. accountData.rows[0]), will provide access to the single object in the rows array.
+
+// Multiple Items Returned
+// [objectName].rows - (e.g. classificationData.rows.foreach(.....))
